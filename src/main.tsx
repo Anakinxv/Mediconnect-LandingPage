a18cb2aee763448ae30d1f4b84b44bd4f6e327e3
Anakinxv <144478975+Anakinxv@users.noreply.github.com>
2025-12-04 13:15:52 -0400

    Primer commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div className='bg-black h-screen w-screen  text-white'>
+
+
+    <App />    </div>
+  </StrictMode>,
+)
